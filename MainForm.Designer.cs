593c4bc7ab9d816412d4f34f7614a74edc67c304
андрей ларин <andrey_larin_03@mail.ru>
2023-12-22 15:17:50 +0300

    Добавьте файлы проекта.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace WinFormsApp1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            AdminButton = new Button();
+            EmlpoyeeButton = new Button();
+            ClientButton = new Button();
+            SuspendLayout();
+            // 
+            // AdminButton
+            // 
+            AdminButton.Location = new Point(53, 168);
+            AdminButton.Name = "AdminButton";
+            AdminButton.Size = new Size(189, 70);
+            AdminButton.TabIndex = 0;
+            AdminButton.Text = "Администратор";
+            AdminButton.UseVisualStyleBackColor = true;
+            AdminButton.Click += AdminButton_Click;
+            // 
+            // EmlpoyeeButton
+            // 
+            EmlpoyeeButton.Location = new Point(313, 168);
+            EmlpoyeeButton.Name = "EmlpoyeeButton";
+            EmlpoyeeButton.Size = new Size(189, 70);
+            EmlpoyeeButton.TabIndex = 1;
+            EmlpoyeeButton.Text = "Сотрудник";
+            EmlpoyeeButton.UseVisualStyleBackColor = true;
+            EmlpoyeeButton.Click += EmlpoyeeButton_Click;
+            // 
+            // ClientButton
+            // 
+            ClientButton.Location = new Point(566, 168);
+            ClientButton.Name = "ClientButton";
+            ClientButton.Size = new Size(189, 70);
+            ClientButton.TabIndex = 2;
+            ClientButton.Text = "Сотрудник";
+            ClientButton.UseVisualStyleBackColor = true;
+            ClientButton.Click += ClientButton_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(ClientButton);
+            Controls.Add(EmlpoyeeButton);
+            Controls.Add(AdminButton);
+            Name = "MainForm";
+            Text = "MainForm";
+            Load += MainForm_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button AdminButton;
+        private Button EmlpoyeeButton;
+        private Button ClientButton;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AdminButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void EmlpoyeeButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void ClientButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
